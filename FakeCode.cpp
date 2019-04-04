26c1a20223b1c249d6654a4d8bcc45833b27036e
Test <gosa2506@usherbrooke.ca>
2019-04-03 20:31:39 -0400

    blablabla

--- FakeCode.cpp
+++ FakeCode.cpp
@@ -4,6 +4,8 @@ int main()
 {
 
 	std::cout << "Hello, not only Montreal!\n";
-	std::cout << "But hi to everyone else, too\n"
+	std::cout << "But hi to everyone else, too\n"\
+	
+	// Bla bla bla
 	return 0;
 }
